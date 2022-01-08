1d20722954a515d678f94302d97c570c62d08ce5
Tilman Krokotsch <t.krokotsch@gmail.com>
2022-01-08 11:16:42 +0100

    Finish hate speech dataset.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,11 +2,14 @@ import unittest
 from unittest import mock
 
 import responses
+import torch
 from responses import matchers
 
 from composing_datasets.dataset import HateSpeechDataset, _download_data
 
 
+@mock.patch("os.makedirs")
+@mock.patch("os.path.exists", return_value=True)
 class TestHateSpeechDataset(unittest.TestCase):
     MOCK_CSV = """,count,hate_speech,offensive_language,neither,class,tweet
         0,3,0,0,3,2,Tweet 1
@@ -14,9 +17,7 @@ class TestHateSpeechDataset(unittest.TestCase):
         2,3,0,3,0,0,Tweet 3"""
 
     @mock.patch("composing_datasets.dataset._download_data")
-    @mock.patch("os.makedirs")
-    @mock.patch("os.path.exists")
-    def test_download(self, mock_exists, mock_makedirs, mock_download):
+    def test_download(self, mock_download, mock_exists, mock_makedirs):
         with self.subTest("not downloaded"):
             mock_exists.return_value = False
             HateSpeechDataset()
@@ -39,15 +40,35 @@ class TestHateSpeechDataset(unittest.TestCase):
             mock_exists.assert_called_with(HateSpeechDataset.DATA_FILE)
             mock_download.assert_not_called()
 
-    @mock.patch("composing_datasets.dataset._download_data")
-    @mock.patch("os.makedirs")
-    @mock.patch("os.path.exists", return_value=True)
-    def test_loading_data(self, mock_exists, mock_makedirs, mock_download):
+    def test_loading_data(self, mock_exists, mock_makedirs):
         mock_open = mock.mock_open(read_data=self.MOCK_CSV)
         with mock.patch("composing_datasets.dataset.open", new=mock_open):
             dataset = HateSpeechDataset()
             self.assertListEqual([0, 1, 1], dataset.labels)
-            self.assertListEqual(["Tweet 1", "Tweet 2", "Tweet 3"], dataset.text)
+            self.assertListEqual(
+                [["tweet", "1"], ["tweet", "2"], ["tweet", "3"]], dataset.text
+            )
+
+    def test_get_item(self, mock_exists, mock_makedirs):
+        mock_open = mock.mock_open(read_data=self.MOCK_CSV)
+        with mock.patch("composing_datasets.dataset.open", new=mock_open):
+            dataset = HateSpeechDataset()
+            expected_samples = [
+                (torch.tensor([0, 1]), torch.tensor(0)),
+                (torch.tensor([0, 2]), torch.tensor(1)),
+                (torch.tensor([0, 3]), torch.tensor(1)),
+            ]
+            for (exp_ids, exp_label), (ids, label) in zip(expected_samples, dataset):
+                self.assertTrue(torch.all(exp_ids == ids))
+                self.assertEqual(exp_label, label)
+                self.assertEqual(torch.long, ids.dtype)
+                self.assertEqual(torch.long, label.dtype)
+
+    def test_len(self, mock_exists, mock_makedirs):
+        mock_open = mock.mock_open(read_data=self.MOCK_CSV)
+        with mock.patch("composing_datasets.dataset.open", new=mock_open):
+            dataset = HateSpeechDataset()
+            self.assertEqual(3, len(dataset))
 
 
 class TestDownloadData(unittest.TestCase):
